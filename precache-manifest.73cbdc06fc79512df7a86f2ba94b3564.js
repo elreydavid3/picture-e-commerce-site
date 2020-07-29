61300373dc2017c721fdbf4118f4ab619a38dd32
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-07-28 19:22:15 -0600

    Updates

---
+++ precache-manifest.73cbdc06fc79512df7a86f2ba94b3564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f004b4989213daf398203de957dee471",
+    "url": "/picture-e-commerce-site/index.html"
+  },
+  {
+    "revision": "2eeaf110db772f616e01",
+    "url": "/picture-e-commerce-site/static/js/2.f24b8a74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/picture-e-commerce-site/static/js/2.f24b8a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6a6a7f19fa19f83813",
+    "url": "/picture-e-commerce-site/static/js/main.58c6321b.chunk.js"
+  },
+  {
+    "revision": "8f2bbe8dc1a9df637e2b",
+    "url": "/picture-e-commerce-site/static/js/runtime-main.fccfc0ae.js"
+  }
+]);
